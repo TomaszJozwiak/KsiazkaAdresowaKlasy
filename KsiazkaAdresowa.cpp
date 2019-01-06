8b6fc3f205d6c145667a880ba3e4ba434cbf66d6
TomaszJóźwiak <tomasz.jozwiak.programista@gmail.com>
2019-01-06 16:11:16 +0100

    Added load user to vector on start program

--- KsiazkaAdresowa.cpp
+++ KsiazkaAdresowa.cpp
@@ -1,5 +1,12 @@
 #include "KsiazkaAdresowa.h"
 
+
+KsiazkaAdresowa::KsiazkaAdresowa()
+{
+    uzytkownikMenedzer.wczytajUzytkownikowZPliku();
+}
+
+
 void KsiazkaAdresowa::rejestracjaUzytkownika()
 {
     uzytkownikMenedzer.rejestracjaUzytkownika();
